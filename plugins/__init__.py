0ee299d4455afc6ce838082cd3a1f79d48ca67fc
MR GOKU <154590485+VIPBOLTE@users.noreply.github.com>
2025-02-12 19:15:25 +0530

    Update __init__.py

--- plugins/__init__.py
+++ plugins/__init__.py
@@ -15,5 +15,5 @@ def __list_all_modules():
     return all_modules
 
 
-ALL_MODULES = sorted(__list_all_modules())
-__all__ = ALL_MODULES + ["ALL_MODULES"]
+PLUGINS_MODULES = sorted(__list_all_modules())
+__all__ = PLUGINS_MODULES + ["PLUGINS_MODULES"]
