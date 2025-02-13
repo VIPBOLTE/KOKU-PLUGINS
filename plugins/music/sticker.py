f6e547fb0f73fc3bfd4fc706de880aae50787c9c
MR GOKU <154590485+VIPBOLTE@users.noreply.github.com>
2025-02-13 13:20:58 +0530

    Update sticker.py

--- plugins/music/sticker.py
+++ plugins/music/sticker.py
@@ -21,7 +21,7 @@ from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, Message
 from KOKUMUSIC import app
 from utils.error import capture_err
 
-BOT_USERNAME = app.username
+BOT_USERNAME = app.me.username
 
 MAX_STICKERS = (
     120  # would be better if we could fetch this limit directly from telegram
