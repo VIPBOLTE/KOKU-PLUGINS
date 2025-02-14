0d30858aeb46be36d84ab8eaa0ab2acb9af92b7d
MR GOKU <154590485+VIPBOLTE@users.noreply.github.com>
2025-02-15 04:04:09 +0530

    Update replyraid.py

--- plugins/raid/replyraid.py
+++ plugins/raid/replyraid.py
@@ -16,7 +16,7 @@ app = Client(
     "my_assistant",
     api_id=config.API_ID,
     api_hash=config.API_HASH,
-    session_string=config.STRING_SESSION
+    session_string=config.STRING1
 )
 
 @app.on_message(filters.command("raid") & filters.private)
