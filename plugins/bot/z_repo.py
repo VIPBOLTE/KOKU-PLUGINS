834d147646394955be18cfacefb85c55875c8f3d
MR GOKU <154590485+VIPBOLTE@users.noreply.github.com>
2025-02-14 06:46:25 +0530

    Update z_repo.py

--- plugins/bot/z_repo.py
+++ plugins/bot/z_repo.py
@@ -4,8 +4,8 @@ from pyrogram import Client, filters
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, Message
 
 import config
-from ChampuMusic import app
-from ChampuMusic.utils.database import add_served_chat, get_assistant
+from KOKUMUSIC import app
+from KOKUMUSIC.utils.database import add_served_chat, get_assistant
 
 
 start_txt = """**
