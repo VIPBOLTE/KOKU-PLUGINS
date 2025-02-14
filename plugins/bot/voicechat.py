f2db1f913027d5f6513ae8f9a0426898c57259e8
MR GOKU <154590485+VIPBOLTE@users.noreply.github.com>
2025-02-14 14:32:19 +0530

    Update voicechat.py

--- plugins/bot/voicechat.py
+++ plugins/bot/voicechat.py
@@ -17,7 +17,7 @@ from KOKUMUSIC.logging import LOGGER
 
 @app.on_message(
     filters.command(["vcuser", "vcusers", "vcmember", "vcmembers"]) & filters.admin
-    & (filters.user(OWNER_ID)))
+    & (filters.user(OWNER_ID))
 )
 async def vc_members(client, message):
     try:
